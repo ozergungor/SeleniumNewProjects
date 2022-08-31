c382846bc6bb0cb77add8e79a176faf508168029
ozergungor <ozergungor2019@gmail.com>
2022-08-31 09:28:45 -0400

    TestNG_Intro

--- src/test/java/com/cydeo/tests/day05_testNG_Intro_Dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day05_testNG_Intro_Dropdowns/TestNG_Intro.java
@@ -3,13 +3,15 @@ package com.cydeo.tests.day05_testNG_Intro_Dropdowns;
 import org.testng.annotations.Test;
 
 public class TestNG_Intro {
+    @Test
+    public void test1(){
+        System.out.println("Test1 is running ...");
+    }
 
     @Test
+    public void test2(){
 
-    public void test1(){
-
-        System.out.println("Test1 is running ...");
-
+        System.out.println("Test2 is running ...";
 
     }
 
