6bda1dfa238f2c09bfbcfca07aa000891c3885c9
ozergungor <ozergungor2019@gmail.com>
2022-10-02 15:37:01 -0400

    Day-11 T1_Circle_Drag_And_Drop

---
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T1_Circle_Drag_And_Drop.java
@@ -0,0 +1,16 @@
+package com.cydeo.tests.day11_actions_jsexecutor_practice;
+
+import org.testng.annotations.Test;
+
+public class T1_Circle_Drag_And_Drop {
+
+    @Test
+    public void drag_drop_test(){
+
+        //TC #: Drag and drop
+        //1. Go to https://demos.telerik.com/kendo-ui/dragdrop/index
+        //2. Drag and drop the small circle to bigger circle.
+        //3. Assert:
+        //-Text in big circle changed to: “You did great!”
+    }
+}
