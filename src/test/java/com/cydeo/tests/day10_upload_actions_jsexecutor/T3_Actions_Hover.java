977da72e62201a0fa23f1c6f58cd199f6aa60c4f
ozergungor <ozergungor2019@gmail.com>
2022-10-02 06:24:03 -0400

    Day-11 T7_Scroll_JSExecutor

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T3_Actions_Hover.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T3_Actions_Hover.java
@@ -1,12 +1,14 @@
 package com.cydeo.tests.day10_upload_actions_jsexecutor;
 
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class T3_Actions_Hover {
-
     @Test
     public void hovering_test(){
 
@@ -24,19 +26,30 @@ public class T3_Actions_Hover {
         WebElement user2 = Driver.getDriver().findElement(By.xpath("//h5[.='name: user2']"));
         WebElement user3 = Driver.getDriver().findElement(By.xpath("//h5[.='name: user3']"));
 
-        //2. Hover over to first image
+        Actions actions = new Actions(Driver.getDriver());
 
+        //2. Hover over to first image
+        BrowserUtils.sleep(2);
+        actions.moveToElement(img1).perform();
 
         //3. Assert:
         //a. “name: user1” is displayed
-        //b. “view profile” is displayed
+        Assert.assertTrue(user1.isDisplayed());
+
         //4. Hover over to second image
+        BrowserUtils.sleep(2);
+        actions.moveToElement(img2).perform();
+
         //5. Assert:
         //a. “name: user2” is displayed
-        //b. “view profile” is displayed
+        Assert.assertTrue(user2.isDisplayed());
+
         //6. Hover over to third image
+        BrowserUtils.sleep(2);
+        actions.moveToElement(img3).perform();
+
         //7. Confirm:
         //a. “name: user3” is displayed
-        //b. “view profile” is displayed
+        Assert.assertTrue(user3.isDisplayed());
     }
 }
