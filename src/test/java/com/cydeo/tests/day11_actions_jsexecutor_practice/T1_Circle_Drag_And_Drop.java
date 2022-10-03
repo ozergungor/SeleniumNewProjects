fb8d3bb66e153e52dd6e937135c9a27769ddcba4
ozergungor <ozergungor2019@gmail.com>
2022-10-03 10:39:21 -0400

    Day-12 T1_LibraryLoginPage

--- src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T1_Circle_Drag_And_Drop.java
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T1_Circle_Drag_And_Drop.java
@@ -1,5 +1,10 @@
 package com.cydeo.tests.day11_actions_jsexecutor_practice;
 
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class T1_Circle_Drag_And_Drop {
@@ -9,8 +14,22 @@ public class T1_Circle_Drag_And_Drop {
 
         //TC #: Drag and drop
         //1. Go to https://demos.telerik.com/kendo-ui/dragdrop/index
+        Driver.getDriver().get("https://demos.telerik.com/kendo-ui/dragdrop/index");
+        WebElement acceptCookiesButton = Driver.getDriver().findElement(By.id("onetrust-accept-btn-handler"));
+        acceptCookiesButton.click();
+
         //2. Drag and drop the small circle to bigger circle.
+        WebElement smallcircle = Driver.getDriver().findElement(By.xpath("//div[@id='draggable']"));
+        WebElement bigCircle = Driver.getDriver().findElement(By.xpath("//div[@id='droptarget']"));
+
+        Actions actions = new Actions(Driver.getDriver());
+        actions.dragAndDrop(smallcircle, bigCircle).perform();
+
         //3. Assert:
         //-Text in big circle changed to: “You did great!”
+
+        String actual = bigCircle.getText();
+        String expected = "You did great!";
+        Assert.assertEquals(actual, expected);
     }
 }
