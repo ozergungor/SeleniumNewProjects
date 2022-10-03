f2013a9a270923fb4b647f3e7b5bb266c02309d6
ozergungor <ozergungor2019@gmail.com>
2022-10-03 15:29:03 -0400

    Day-12 POMPractices

---
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractices.java
@@ -0,0 +1,13 @@
+package com.cydeo.tests.day12_pom_design_explicit_wait;
+
+import org.testng.annotations.Test;
+
+public class POMPractices {
+
+    @Test
+    public void  required_field_error_message_test(){
+
+
+
+    }
+}
